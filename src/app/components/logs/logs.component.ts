194291cdf77d6d86d88f662d74f2891fad9944e5
LM1 <majcher.luk@gmail.com>
2018-03-14 18:45:51 +0100

    add edit delete log

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -11,17 +11,33 @@ import { tick } from '@angular/core/testing';
 })
 export class LogsComponent implements OnInit {
   logs: Log[];
+  selectedLog: Log;
+  loaded: boolean = false;
 
   constructor(private logService: LogService) { }
 
   ngOnInit() {
+    this.logService.stateClear.subscribe(clear => {
+      if(clear) {
+        this.selectedLog = {id: '', text: '', date: ''};
+      }
+    });
+
     this.logService.getLogs().subscribe(logs => {
       this.logs = logs;
+      this.loaded = true;
     });
   }
 
-  onSelect(log: Log){
+  onSelect(log: Log) {
     this.logService.setFormLog(log);
+    this.selectedLog = log;
+  }
+
+  onDelete(log: Log) {
+    if(confirm('Are you sure?')){
+      this.logService.deleteLog(log);
+    }
   }
 
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -12,6 +12,9 @@ export class LogService {
   private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+  
   constructor() { 
     
     this.logs = [
@@ -27,6 +30,31 @@ export class LogService {
 
   setFormLog(log: Log){
     this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
+
+  clearState() {
+    this.stateSource.next(true);
   }
 
 }
